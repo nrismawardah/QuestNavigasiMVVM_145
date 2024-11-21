093edebdd336bcce0669c3afbad146ddc206f405
nrismawardah <nrismawardah@gmail.com>
2024-11-21 21:55:31 +0700

    Commit 24: Mendeklarasikan properti _statusUI yang berfungsi untuk menyimpan dan memperbarui data data siswa dengan menggunakan StateFlow

--- app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.questnavigasimvvm.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.questnavigasimvvm.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class SiswaViewModel : ViewModel() {
+class SiswaViewModel : ViewModel()
+{
+    private val _statusUI = MutableStateFlow(DataSiswa()
+    )
 }
