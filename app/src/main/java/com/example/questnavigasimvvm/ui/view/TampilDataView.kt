21ee32361b6bf6dac0aa0aa6aa262216ec2e2231
nrismawardah <nrismawardah@gmail.com>
2024-11-21 21:41:48 +0700

    Commit 19: Membuat layout Row untuk menyusun elemen-elemen di dalam Row secara horizontal, mengisi seluruh ruang yang tersedia, serta mengatur elemen-elemennya secara horizontal yang memastikan bahwa elemen pertama akan ditempatkan di posisi paling kiri dan elemen terakhir di posisi paling kanan

--- app/src/main/java/com/example/questnavigasimvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/questnavigasimvvm/ui/view/TampilDataView.kt
@@ -1,7 +1,10 @@
 package com.example.questnavigasimvvm.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,6 +43,12 @@ fun TampilData(
         modifier = Modifier
             .padding(16.dp)
     ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
 
+        }
     }
 }
