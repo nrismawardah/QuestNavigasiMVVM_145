2e991a578a4fb364dfffe4c93c9a884d80e0f328
nrismawardah <nrismawardah@gmail.com>
2024-11-21 21:32:39 +0700

    Commit 14: Membuat composable function TampilDataView dengan parameter modifier, uiState yang berisi DataSiswa untuk ditampilkan, serta onBackButton yang menangani aksi ketika button "Kembali" ditekan

---
+++ app/src/main/java/com/example/questnavigasimvvm/ui/view/TampilDataView.kt
@@ -0,0 +1,14 @@
+package com.example.questnavigasimvvm.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.questnavigasimvvm.model.DataSiswa
+
+@Composable
+fun TampilDataView(
+    modifier: Modifier = Modifier,
+    uiState: DataSiswa,
+    onBackButton: () -> Unit
+){
+
+}
