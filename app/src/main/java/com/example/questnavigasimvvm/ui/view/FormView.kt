df9cda2f6ab583f0e7c1bc115670e5f0b6d6ab73
nrismawardah <nrismawardah@gmail.com>
2024-11-21 20:59:34 +0700

    Commit 5: Membuat composable function FormView dengan parameter modifier, listJK berupa data string dari jenis kelamin, serta onSubmitClicked yang menangkap data input dari pengguna dan mengirimkannya ke pemanggil fungsi

---
+++ app/src/main/java/com/example/questnavigasimvvm/ui/view/FormView.kt
@@ -0,0 +1,13 @@
+package com.example.questnavigasimvvm.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+){
+
+}
