a78ba391d7170b3b395e8ff040e6b96a95c05c49
nrismawardah <nrismawardah@gmail.com>
2024-11-21 21:57:33 +0700

    Commit 25: Mengubah MutableStateFlow yang dapat diubah menjadi StateFlow yang hanya bisa dibaca

--- app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
@@ -3,9 +3,12 @@ package com.example.questnavigasimvvm.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.questnavigasimvvm.model.DataSiswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SiswaViewModel : ViewModel()
 {
-    private val _statusUI = MutableStateFlow(DataSiswa()
-    )
+    private val _statusUI = MutableStateFlow(DataSiswa())
+
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
 }
