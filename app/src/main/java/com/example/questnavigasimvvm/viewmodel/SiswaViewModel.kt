da4ba35e68a366d377ee01ca2b23306b9f42ca70
nrismawardah <nrismawardah@gmail.com>
2024-11-21 22:00:11 +0700

    Commit 26: Membuat function saveDataSiswa untuk memperbarui data siswa di dalam MutableStateFlow dengan informasi yang diberikan melalui MutableList<String>

--- app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm/viewmodel/SiswaViewModel.kt
@@ -5,10 +5,24 @@ import com.example.questnavigasimvvm.model.DataSiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel()
 {
     private val _statusUI = MutableStateFlow(DataSiswa())
 
     val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun saveDataSiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2],
+                alamat = ls[3],
+                notelp = ls[4],
+                gender = ls[5]
+            )
+        }
+    }
 }
