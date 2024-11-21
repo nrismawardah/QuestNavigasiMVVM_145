ed42d07df750374d77aa65d671c5a412b55d65e0
nrismawardah <nrismawardah@gmail.com>
2024-11-21 21:02:20 +0700

    Commit 6: Mendeklarasikan variabel-variabel yang menyimpan data input pengguna seperti nim, nama, email, alamat, notelp, dan gender dalam sebuah form

--- app/src/main/java/com/example/questnavigasimvvm/ui/view/FormView.kt
+++ app/src/main/java/com/example/questnavigasimvvm/ui/view/FormView.kt
@@ -1,6 +1,10 @@
 package com.example.questnavigasimvvm.ui.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -9,5 +13,10 @@ fun FormView(
     listJK: List<String>,
     onSubmitClicked: (MutableList<String>) -> Unit
 ){
-
+    var nim by remember { mutableStateOf("") }
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var notelp by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
 }
