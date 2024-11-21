abd41a20d81a55e9b1f41792fd22c4f8ec78c3b5
nrismawardah <nrismawardah@gmail.com>
2024-11-21 22:20:44 +0700

    Commit 32: Membuat komponen FormView dengan parameter listJK berupa list yang digunakan untuk menampilkan data dan parameter onSubmitClicked yang akan dipanggil ketika tombol submit ditekan

--- app/src/main/java/com/example/questnavigasimvvm/NavigationController.kt
+++ app/src/main/java/com/example/questnavigasimvvm/NavigationController.kt
@@ -2,14 +2,20 @@ package com.example.questnavigasimvvm
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasimvvm.model.JenisKelamin.listJK
+import com.example.questnavigasimvvm.ui.view.FormView
 import com.example.questnavigasimvvm.viewmodel.SiswaViewModel
+
 
 enum class Halaman{
     FORMULIR,
@@ -17,11 +23,14 @@ enum class Halaman{
 }
 
 @Composable
-fun NavigationController(
+fun NavigationControl(
     modifier: Modifier = Modifier,
     viewModel: SiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
 ){
+    val context = LocalContext.current
+    val uiState by viewModel.statusUI.collectAsState()
+
     NavHost(
         modifier = modifier.padding(16.dp),
         navController = navHost,
@@ -30,7 +39,15 @@ fun NavigationController(
         composable(
             route = Halaman.FORMULIR.name
         ){
-
+            FormView(
+                listJK = listJK.map { id ->
+                    context.getString(id)
+                },
+                onSubmitClicked = {
+                    viewModel.saveDataSiswa(it)
+                    navHost.navigate(Halaman.TAMPILDATA.name)
+                }
+            )
         }
     }
 }
