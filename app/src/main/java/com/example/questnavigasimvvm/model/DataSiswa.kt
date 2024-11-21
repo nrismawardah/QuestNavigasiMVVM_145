1ab73945135bc129288889916fd500ba40d0c7af
nrismawardah <nrismawardah@gmail.com>
2024-11-21 20:38:32 +0700

    Commit 2: Menambahkan data class DataSiswa ke dalam package model yang berisi nim, nama, email, alamat, notelp, dan gender bertipe string menggunakan variable val agar nilainya tidak bisa diubah setelah inisialisasi

---
+++ app/src/main/java/com/example/questnavigasimvvm/model/DataSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm.model
+
+data class DataSiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val email: String = "",
+    val alamat: String = "",
+    val notelp: String = "",
+    val gender: String = "",
+)
