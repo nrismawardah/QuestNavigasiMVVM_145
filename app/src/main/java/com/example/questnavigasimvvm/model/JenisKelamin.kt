e7258d1a622a29f282b4c08968faab96c0decbd5
nrismawardah <nrismawardah@gmail.com>
2024-11-21 20:50:26 +0700

    Commit 4: Membuat sebuah singleton object bernama JenisKelamin yang menyimpan daftar referensi string untuk jenis kelamin menggunakan variable val bertipe listJK

---
+++ app/src/main/java/com/example/questnavigasimvvm/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm.model
+
+import com.example.questnavigasimvvm.R
+
+object JenisKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
