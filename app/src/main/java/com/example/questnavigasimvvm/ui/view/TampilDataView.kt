28697a6f94b4eaab267ebd27ec017aae6c35ec3a
nrismawardah <nrismawardah@gmail.com>
2024-11-21 21:33:39 +0700

    Commit 15: Membuat layout Column untuk menyusun elemen-elemen di dalam Column secara vertikal, mengisi seluruh ruang yang tersedia, memberi padding sebesar 16.dp, serta memusatkan elemen-elemennya secara horizontal,

--- app/src/main/java/com/example/questnavigasimvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/questnavigasimvvm/ui/view/TampilDataView.kt
@@ -1,7 +1,12 @@
 package com.example.questnavigasimvvm.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasimvvm.model.DataSiswa
 
 @Composable
@@ -10,5 +15,12 @@ fun TampilDataView(
     uiState: DataSiswa,
     onBackButton: () -> Unit
 ){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
 
+    }
 }
