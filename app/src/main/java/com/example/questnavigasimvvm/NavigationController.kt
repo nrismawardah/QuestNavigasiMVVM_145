7e4c094d69f82c2d98f45aa2bae75a634292c16e
nrismawardah <nrismawardah@gmail.com>
2024-11-21 22:08:53 +0700

    Commit 29: Membuat composable function NavigationController dengan parameter modifier untuk menyesuaikan tampilan UI, parameter viewModel untuk menangani logika bisnis dan data, serta parameter navHost untuk menangani navigasi

--- app/src/main/java/com/example/questnavigasimvvm/NavigationController.kt
+++ app/src/main/java/com/example/questnavigasimvvm/NavigationController.kt
@@ -1,6 +1,22 @@
 package com.example.questnavigasimvvm
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasimvvm.viewmodel.SiswaViewModel
+
 enum class Halaman{
     FORMULIR,
     TAMPILDATA
+}
+
+@Composable
+fun NavigationController(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){
+
 }
